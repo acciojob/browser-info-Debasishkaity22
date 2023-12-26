e0881e0d9a4b1cade62db0d234a9c7fbe29ae0ab
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-26 17:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let browserInfo=document.getElementById("browser-info");
+document.addEventListener("DOMContentLoaded",function(){
+	let user=navigator.userAgent
+	let browserName=navigator.appName;
+	let appVersion=navigator.appVersion;
+	browserInfo.innerText="You are using "+browserName+" version "+appVersion;
+})
